ef0b1627418e984e39226d38c3c3fd786db832f0
Wojciech Cinal <wojtekc44@gmail.com>
2023-12-11 21:27:25 +0100

    Ver. 0.0.8
    Changes into categoryList and header improvement

--- resources/views/Category/categoryList.blade.php
+++ resources/views/Category/categoryList.blade.php
@@ -79,15 +79,15 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Your Categories') }}</div>
+                    <div class="card-header">{{ __('Moje kategorie') }}</div>
 
                     <div class="card-body">
-                        <h4>Categories:</h4>
                         <table class="table">
                             <thead>
                                 <tr>
-                                    <th>Name</th>
+                                    <th>Nazwa kategorii</th>
                                     <th>Opcje</th>
+                                    <th>Podkategorie</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -97,35 +97,22 @@
                                         <td>
                                             <button class="btn btn-primary btn-sm edit-category"
                                                 data-id="{{ $category->id_category }}">Edytuj</button>
+
+                                        <td>
+                                            @foreach ($subcategories as $subcategory)
+                                                @if ($category->id_category === $subcategory->id_category)
+                                                    <li><a>{{ $subcategory->name_subCategory }}</a>
+                                                    </li>
+                                                @endif
+                                            @endforeach
+                                        </td>
+
                                         </td>
                                     </tr>
                                 @endforeach
                             </tbody>
                         </table>
-                    </div>
-                </div>
 
-                <div class="card mt-4">
-                    <div class="card-header">{{ __('Your Subcategories') }}</div>
-
-                    <div class="card-body">
-                        <h4>Subcategories:</h4>
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Category</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($subcategories as $subcategory)
-                                    <tr>
-                                        <td>{{ $subcategory->name_subCategory }}</td>
-                                        <td>{{ $subcategory->category->name_category }}</td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
                     </div>
                 </div>
             </div>
